a204b3bdc5ea57aa9f3e984860615127b0119287
yuna237 <wh1854wh@naver.com>
2022-09-13 17:32:28 +0900

    20220913

---
+++ Dodge/Assets/Plater controller.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platercontroller : MonoBehaviour
+{
+    public Rigidbody PlayerRigidbody;//이동에 사용할 리지드바디 컴포넌트
+    public float speed = 8f;//이동 속력
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
