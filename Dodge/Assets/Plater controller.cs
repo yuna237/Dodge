56a284e2a6a494c09d35b3638811f993a2b24398
yuna237 <wh1854wh@naver.com>
2022-09-14 17:37:39 +0900

    20220914

--- Dodge/Assets/Plater controller.cs
+++ Dodge/Assets/Plater controller.cs
@@ -8,12 +8,35 @@ public class Platercontroller : MonoBehaviour
     public float speed = 8f;//이동 속력
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            //위쪽 방향키 입력이 감지됭 경우 z 방향 힘주기
+            PlayerRigidbody.AddForce(0f, 0f, speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            // 아래쪽 방향키 입력이 감지된 경우 -z 방향 힘주기
+            PlayerRigidbody.AddForce(0f, 0f, -speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            //오른쪽 방향키 입력이 감지된 경우 x 방향감 주기
+            PlayerRigidbody.AddForce(speed, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            //왼쪽 방향키 입력이 감지된 경우 -x 방향 힘 주기
+            PlayerRigidbody.AddForce(-speed, 0f, 0f);
+        }
+           
     }
+     
+        
+
 }
