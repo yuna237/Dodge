b936b63ecd97bebb69011e0f9181cf7a8d2f6079
yuna237 <wh1854wh@naver.com>
2022-09-16 12:37:18 +0900

    20220916.01

--- Dodge/Assets/Platercontroller.cs
+++ Dodge/Assets/Platercontroller.cs
@@ -16,27 +16,20 @@ public class Platercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            //위쪽 방향키 입력이 감지됭 경우 z 방향 힘주기
-            PlayerRigidbody.AddForce(0f, 0f, speed);
-        }
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            // 아래쪽 방향키 입력이 감지된 경우 -z 방향 힘주기
-            PlayerRigidbody.AddForce(0f, 0f, -speed);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            //오른쪽 방향키 입력이 감지된 경우 x 방향감 주기
-            PlayerRigidbody.AddForce(speed, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            //왼쪽 방향키 입력이 감지된 경우 -x 방향 힘 주기
-            PlayerRigidbody.AddForce(-speed, 0f, 0f);
-        }
-    
+        //수평축과 수직축의 입력값을 감지하여 저장
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        //실제 이동 속도를 입력값과 이동 속력을 시용해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        //Vector3 속도를 (xSpeed, 0, zSpeed)로 생성
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        //리지드바디의 속도에 newVelocity 할당
+        PlayerRigidbody.velocity = newVelocity;
+
+
     }
     public void Die() 
     {
