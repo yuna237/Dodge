164ab4262d1ad0b311b5655471d66ea4e584fac8
yuna237 <wh1854wh@naver.com>
2022-09-19 17:35:52 +0900

    20220919.2

--- Dodge/Assets/Bullet.cs
+++ Dodge/Assets/Bullet.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 8f;//탄알 이동 속력
-    private Rigidbody bulletRigidbody;//이동에 사용할 리지드바디 컴포넌트
+    //이동에 사용할 리지드바디 컴포넌트
+    private Rigidbody bulletRigidbody;
+    //탄알 이동 속력
+    public float speed = 8f;
 
     void Start()
     {
@@ -13,6 +15,27 @@ public class Bullet : MonoBehaviour
         bulletRigidbody = GetComponent<Rigidbody>();
         //리지드바디의 속도 = 앞쪽 방향 * 이동 속력
         bulletRigidbody.velocity = transform.forward * speed;
+
+        //3초 뒤에 자신의 게임 오프젝트 파괴
+        Destroy(gameObject, 3f);
+    }
+
+    //트리거 충돌 시 자동으로 실행되는 메서드
+    private void OnTriggerEnter(Collider other)
+    {
+        //충돌한 상대방 게임 오브젝트가 Plater 태그를 가진 경우
+        if (other.tag == "player")
+        {
+            //상대방 게임 오브젝트에서 PlayerController 컴포넌트 가저오기
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            //상대방으로부터 PlayerController 컴포넌트 가져오는 데 성공 했다면
+            if (playerController != null)
+            {
+                //상대방 PlayerController 컴포넌트 Die() 메서드 실행
+                playerController.Die();
+            }
+        }
     }
 
 
