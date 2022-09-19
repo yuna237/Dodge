eee170bb3981ae353f342bb0a7ae340c5c005f2e
yuna237 <wh1854wh@naver.com>
2022-09-19 12:38:31 +0900

    20220919.1

---
+++ Dodge/Assets/Bullet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;//탄알 이동 속력
+    private Rigidbody bulletRigidbody;//이동에 사용할 리지드바디 컴포넌트
+
+    void Start()
+    {
+        //게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 bulletRigidbody에 할당
+        bulletRigidbody = GetComponent<Rigidbody>();
+        //리지드바디의 속도 = 앞쪽 방향 * 이동 속력
+        bulletRigidbody.velocity = transform.forward * speed;
+    }
+
+
+}
