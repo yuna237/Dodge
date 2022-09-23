ce81b78324dc17d4ab200570179062a2b64ac631
yuna237 <wh1854wh@naver.com>
2022-09-23 17:30:40 +0900

    20220923.2

--- Dodge/Assets/02.Scripts/GameManager.cs
+++ Dodge/Assets/02.Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour
 {
     public GameObject gameoverText; // 게임오버 시 활성화할 텍스트 게임 오브젝트
     public TextMeshProUGUI timeTexet; // 생존 시간을 표시할 텍스트 컴포넌트
-    public TextMeshProUGUI recodtdText; // 최고 기록을 표시할 텍스트 컴포넌트
+    public TextMeshProUGUI recordText; // 최고 기록을 표시할 텍스트 컴포넌트
 
 
     private float surviveTime; // 생존 시간
@@ -51,5 +51,19 @@ public class GameManager : MonoBehaviour
         isGameover = true;
         //게임오버 텍스트 게임 오브젝트를 활성화
         gameoverText.SetActive(true);
+
+        //BestTime 키로 저장된 이전까지의 최고 기록 가져오기
+        float bestTime = PlayerPrefs.GetFloat("BestTime");
+
+        //이전까지의 최고 기록보다 현재 생존 시간이 더 크다면
+        if (surviveTime > bestTime)
+        {
+            //최고 기록 값을 현재 생존 시간 값으로 변경
+            bestTime = surviveTime;
+            //변경된 최고 기록을 BestTime 키로 저장
+            PlayerPrefs.SetFloat("BestTime", bestTime);
         }
+        //최고 기록을 recordText 텍스트 컴포넌트를 이용해 표시
+        recordText.text = "BestTime:" + (int)bestTime;
+    }
 }
